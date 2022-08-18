19a61328ac86c7066016a236e75fc36595406c4e
matheus.dimer <matheus.dimer@betha.com.br>
2022-08-17 22:15:03 -0300

    Tratamento padrão para exceptions não tratadas.

--- src/main/java/com/serasa/testetecnico/exception/AppExceptionHandler.java
+++ src/main/java/com/serasa/testetecnico/exception/AppExceptionHandler.java
@@ -1,15 +1,24 @@
 package com.serasa.testetecnico.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class AppExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ApiError> handleEntityNotFound(EntityNotFoundException exception) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<Void> handleEntityNotFound(EntityNotFoundException exception) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception exception) {
+        LOGGER.error(exception.getMessage(), exception);
+        return ResponseEntity.internalServerError().body(new ApiError("Ocorreu um erro interno", 500));
     }
 
 }
